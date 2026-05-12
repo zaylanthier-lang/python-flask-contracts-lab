1a4d7461d0177965286f3358015200d60ebda5b1
Zayden <zaylanthier-lang>
2026-05-12 16:39:12 -0400

    Finished Code

--- server/app.py
+++ server/app.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, current_app, g, make_response
+from flask import Flask, make_response
 
-contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
-customers = ["bob","bill","john","sarah"]
+contracts = [
+    {"id": 1, "contract_information": "This contract is for John and building a shed"},
+    {"id": 2, "contract_information": "This contract is for a deck for a buisiness"},
+    {"id": 3, "contract_information": "This contract is to confirm ownership of this car"}
+]
+
+customers = ["bob", "bill", "john", "sarah"]
+
 app = Flask(__name__)
+
+
+@app.route('/contract/<int:id>')
+def contract_by_id(id):
+    for contract in contracts:
+        if contract["id"] == id:
+            return make_response(contract, 200)
+
+    return make_response({"error": "Contract not found"}, 404)
+
+
+@app.route('/customer/<customer_name>')
+def customer_by_name(customer_name):
+    if customer_name.lower() in customers:
+        return make_response("", 204)
+
+    return make_response({"error": "Customer not found"}, 404)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
